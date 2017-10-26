a9ab4f9a976c379b0b4b9c4e1772f7e430740c7f
Ragavan <ragavanpv@revature.com>
2017-10-26 11:10:19 +0530

    Play Pause done

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ player.on('pause', pauseEvent);
 
 player.on('ended', endEvent);
 
-function pauseEvent () {
+function pauseEvent() {
   console.log('Video Paused');
 }
-function playEvent () {
+
+function playEvent() {
   console.log('Video Played');
 }
-function endEvent () {
+
+function endEvent() {
   console.log('Video Ended');
 }
 
-var seek = function () {
+function seek() {
   var duration = document.getElementById('seekNumber');
   console.log("Seek To" + duration.value);
   player.setCurrentTime(duration.value).catch(function () {
     console.log('Seeked');
+  });
+}
+
+function play() {
+  player.play().catch(function (error) {
+    console.error('error playing the video:', error.name);
+  });
+}
+
+function pause() {
+  player.pause().catch(function (error) {
+    console.error('error pausing the video:', error.name);
+  });
+}
+
+function currentTime() {
+  player.getCurrentTime().then(function (seconds) {
+    console.log(seconds);
+  }).catch(function (error) {
+    console.error('error getting time of the video:', error.name);
   });
 }
